51e84b5f17ca2ca2d33cec6cf0114e1076b28c20
Sumit Budhiraja <48406445+Sumit-Budhiraja@users.noreply.github.com>
2020-12-09 02:17:30 +0530

    Add files via upload

---
+++ Pairs of Songs With Total Durations Divisible by 60.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numPairsDivisibleBy60(int[] time) {
+        int count = 0;
+        Map<Integer, Integer> map = new HashMap();
+        for(int t : time) {
+            if(t % 60 == 0)
+                count += map.getOrDefault(0, 0);
+            else 
+                count += map.getOrDefault(60 - t % 60, 0);
+            map.put(t % 60, map.getOrDefault(t % 60, 0) + 1);
+        }
+        return count;
+    }
+}
