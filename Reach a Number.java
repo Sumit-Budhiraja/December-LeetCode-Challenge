09777e6ae54164a9b969be94aa966af02b2d8948
Sumit Budhiraja <48406445+Sumit-Budhiraja@users.noreply.github.com>
2020-12-28 22:06:15 +0530

    Add files via upload

--- Reach a Number.java
+++ Reach a Number.java
@@ -1,16 +1,9 @@
 class Solution {
     public int reachNumber(int target) {
         target = Math.abs(target);
-        int step = 0;
-        int sum = 0;
-        while (sum < target) {
-            step++;
-            sum += step;
-        }
-        while ((sum - target) % 2 != 0) {
-            step++;
-            sum += step;
-        }
-        return step;
+        int k = 0;
+        while (target > 0)
+            target -= ++k;
+        return target % 2 == 0 ? k : k + 1 + k%2;
     }
 }
